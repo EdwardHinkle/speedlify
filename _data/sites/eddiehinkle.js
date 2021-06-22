dc6e93114e9a845a98c60a0e3db0e18d6976b72b
Eddie Hinkle <eddie@hinkle.life>
2021-06-22 13:32:02 -0400

    Remove demo sites and add eddiehinkle work

---
+++ _data/sites/eddiehinkle.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "eddiehinkle.com", // optional, falls back to object key
+	description: "Eddie's Website",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "site"
+	},
+	urls: [
+		"https://work.eddiehinkle.com/",
+	]
+};
